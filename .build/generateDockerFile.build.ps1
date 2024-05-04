7a868a113fb356c5ee746b71e785ce6222c00859
scomnewbie <francois.leon@outlook.com>
2024-05-04 16:17:51 +0200

    Update generateDockerFile.build.ps1

--- .build/generateDockerFile.build.ps1
+++ .build/generateDockerFile.build.ps1
@@ -1,18 +1,15 @@
 task generate_latest_dockerfile {
-    $PSNativeCommandUseErrorActionPreference = $true
     $path = Join-Path 'source' 'Files'
     New-DockerFileFromTemplate -WorkingDirectory $path
     if(123 -ne $LASTEXITCODE){exit 1}
 }
 
 task connect_to_docker {
-    $PSNativeCommandUseErrorActionPreference = $true
     Connect-Docker -Username 'scomnewbie' -Password $env:PAT
     if(0 -ne $LASTEXITCODE){exit 1}
 }
 
 task docker_build {
-    $PSNativeCommandUseErrorActionPreference = $true
     $path = Join-Path 'source' 'Files'
     cd $path
     docker build -t admintools:latest .
@@ -20,13 +17,11 @@ task docker_build {
 }
 
 task docker_tag {
-    $PSNativeCommandUseErrorActionPreference = $true
     docker tag admintools:latest ghcr.io/scomnewbie/admintools:latest
     if(0 -ne $LASTEXITCODE){exit 1}
 }
 
 task docker_push {
-    $PSNativeCommandUseErrorActionPreference = $true
     docker push ghcr.io/scomnewbie/admintools:latest
     if(0 -ne $LASTEXITCODE){exit 1}
 }
